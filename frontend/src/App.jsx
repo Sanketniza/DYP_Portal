92cf60246e37b437e3a833948ba7a51cb9ee224c
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-17 00:44:21 +0530

    Update .gitignore and add new lines in App.jsx and main.jsx for improved structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@
 
 import { createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import "./App.css";
+
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,11 +12,13 @@ const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
         <Toaster />
       </PersistGate>
     </Provider>
+    
   </React.StrictMode>,
 )
